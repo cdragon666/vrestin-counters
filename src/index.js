e33810819d43873046076b56e9dc599bfd6823c2
paull <devlawsec@gmail.com>
2025-04-27 23:26:07 +0100

    Fix login flow to main app with ProtectedRoute

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // src/index.js
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import ProtectedRoute from "./ProtectedRoute";
+import "./App.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
